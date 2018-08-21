8cccd8e2b2f7c4cb5386c1593653a67cc767fc81
fvictoria <felipevictoria630@yahoo.com>
2018-08-21 19:58:18 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        Random r = new Random();
+
+        int guess = 0;
+        int answer = r.nextInt(10);
+        boolean win = false;
+
+        while(!win) {
+            System.out.println("enter guess 0-10");
+            guess = S.nextInt();
+            if (guess == answer) {
+                win = true;
+            }
+
+            if (guess < answer) {
+                System.out.println("too low");
+            }
+
+            if (guess > answer) {
+                System.out.println("too high");
+            }
+        }
+
+        System.out.println("you win");
+    }
+}
